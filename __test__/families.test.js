02692da13f9905598c277783a5714615072012d5
padangpanda <ppanda.naran@outlook.com>
2021-02-19 01:46:29 +0700

    [padang] add device register test

---
+++ __test__/families.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest')
+const app = require('../app')
+
+const { sequelize } = require('../models')
+const { queryInterface } = sequelize
+
+let access_token
+
+beforeAll((done) => {
+  // login dulu
+})
+
+afterAll((done) => {
+  queryInterface.bulkDelete('Families')
+  .then(() => done())
+  .catch(err => done(err))
+})
+
+// ADD
+
+describe('Add family success', () => {
+  test('')
+})
